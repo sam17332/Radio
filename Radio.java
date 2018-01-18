f789e30ef46fb4ae7290d6affa003a0ee304b55a
val17102 <32184574+val17102@users.noreply.github.com>
2018-01-18 17:44:13 -0600

    Update Radio.java
    Se agregaron las funciones frecAdelante y frecAtras

--- Radio.java
+++ Radio.java
@@ -4,10 +4,31 @@ private float frecActual;
 private float[] am;
 private float[] fm;
 private boolean estado;
-private boolean amfm;
+private boolean amfm; //true es am, false es fm
 
 public void Radio(){
 
 }
 
+  public String frecAdelante(){
+  if (amfm && (frecActual < 1610.0)){
+    frecActual = frecActual + 10;
+    return (Float.toString(frecActual.0f);
+  } else if(!amfm && (frecActual > 107.9){
+    frecActual = frecActual + 0.2;
+    return (Float.toString(frecActual.1f);
+  }
+    
+  }
+            
+  public String frecAtras(){
+  if (amfm && (frecActual > 530.0)){
+    frecActual = frecActual - 10;
+    return (Float.toString(frecActual.0f);
+  } else if(!amfm && (frecActual > 87.9){
+    frecActual = frecActual - 0.2;
+    return (Float.toString(frecActual.1f);
+  }  
+  }
+  
 }
