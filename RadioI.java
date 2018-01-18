5f27da25c38ffb16e2622c7f0bbc6da55b22eedd
sam17332 <32148906+sam17332@users.noreply.github.com>
2018-01-17 18:27:45 -0600

    Update RadioI.java

--- RadioI.java
+++ RadioI.java
@@ -1,12 +1,12 @@
 public interface RadioI {
-  public String frecAdelante();
-  public String frecAtras();
-  public boolean prender();
-  public boolean apagar();
-  public String cambioFrecuencia();
-  public void guardarEstacionActual(int pos);
-  public String obtenerEstacion(int pos);
-  public String mostrarEstacion();
-  public boolean estaPrendido();
-  public String obtenerEstado();
+  public String frecAdelante(); //avanza en la frecuencia actual
+  public String frecAtras(); //retrocede en la frecuencia actual
+  public boolean prender(); //enciende el radio
+  public boolean apagar(); //apaga el radio
+  public String cambioFrecuencia(); //cambia entre AM y FM y devuelve si esta en "AM" o "FM"
+  public void guardarEstacionActual(int pos); //guarda la estacion actual en la posicion "pos"
+  public String obtenerEstacion(int pos); //se obtiene la estacion guardada en la posicion "pos"
+  public String mostrarEstacion(); //para obtener la estacion actual
+  public boolean estaPrendido(); //para saber si el radio esta prendido 
+  public String obtenerEstado(); //devuelve "AM" o "FM" 
 }
