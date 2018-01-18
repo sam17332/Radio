01318f9f1ed1092f7fda10b92c258e7bce37b8fa
val17102 <32184574+val17102@users.noreply.github.com>
2018-01-17 18:49:22 -0600

    Create Radio.java
    Se crea la clase que implementa la interface RadioI con atributos

---
+++ Radio.java
@@ -0,0 +1,13 @@
+public class Radio implements RadioI {
+
+private float frecActual;
+private float[] am;
+private float[] fm;
+private boolean estado;
+private boolean amfm;
+
+public void Radio(){
+
+}
+
+}
